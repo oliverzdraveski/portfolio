5fa7f01b3dc635e87e03eba4653be8dfa2e239df
oliverzdraveski <122840184+oliverzdraveski@users.noreply.github.com>
2023-03-03 16:59:06 +0100

    Update About.jsx

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className='portfolio__about-p'>
           <p className='paragraph1'>Hi, I`m Oliver, nice to meet you.Please take a look around!</p>
           <div className="vl" />
-          <p className='paragraph2'>I’m a nice fun and friendly person, I’m honest and puncual, I work well in a team but also on my own as I like to set myself goals which I will achieve, I have good listening and communication skills. I have a creative mind and am always up for new challenges. I am well organized and always plan ahead to make sure I manage my time well.</p>
+          <p className='paragraph2'>I’m a nice fun and friendly person, I’m honest and punctual, I work well in a team but also on my own as I like to set myself goals which I will achieve, I have good listening and communication skills. I have a creative mind and am always up for new challenges. I am well organized and always plan ahead to make sure I manage my time well.</p>
         </div>
       </div> 
     </div>
